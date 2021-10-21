cb76a269ce2bff269b02912ec98a292f73136e1a
gboudrie <gallien.boudrie@gmail.com>
2021-10-21 15:29:36 +0200

    solve function upped and tested

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::{fmt::Error, io};
+use log::debug;
+use anyhow::{Result, bail};
 
 macro_rules! parse_input {
     ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
@@ -18,25 +20,45 @@ fn main() {
         diagram.push(line);
     }
 
-    let answer = solve(w, h, diagram);
-    println!("answer: ");
-    for tuple in answer.unwrap() {
-        println!("  {}{}", tuple.0, tuple.1)
+    let answer = solve(&diagram);
+    for tuple in answer {
+        println!("{}{}", tuple.0, tuple.1)
     }
 }
 
-fn solve(width: i32, height: i32, input: Vec<String>) -> Result<Vec<(char, char)>, Error> {
-    Ok(vec![('A', 'A')])
+fn solve(input: &Vec<String>) -> Vec<(char, char)> {
+    let mut answers: Vec<(char, char)> = Vec::new();
+    let (top, splitted_input) = input.split_first().unwrap();
+    let (bottom, diagram) = splitted_input.split_last().unwrap();
+    
+    for (mut x, t) in top.chars().enumerate() {
+        if !t.is_ascii_whitespace() {
+            debug!("starting from {} \n", t);
+            for line in diagram {
+                if x > 0 && line.chars().nth(x - 1).unwrap() == '-' {
+                    x -= 3;
+                    debug!("left, ");
+                }
+                else if x < top.len() - 1 && line.chars().nth(x + 1).unwrap() == '-' {
+                    x += 3;
+                    debug!("right, ");
+                }
+                else { debug!("straight, "); }
+            }
+            debug!("arriving at {} \n", bottom.chars().nth(x).unwrap());
+            answers.push((t, bottom.chars().nth(x).unwrap()));
+        }
+    }
+    answers
 }
 
 #[cfg(test)]
 mod main_tests {
     use crate::solve;
 
+
     #[test]
     fn example_1() {
-        let w = 7;
-        let h = 7;
         let diagram = vec![
             "A  B  C".to_string(),
             "|  |  |".to_string(),
@@ -46,8 +68,8 @@ mod main_tests {
             "|  |  |".to_string(),
             "1  2  3".to_string(),
         ];
-        let answer = solve(w, h, diagram);
-        assert_eq!(answer.unwrap(), vec![
+        let answer = solve(&diagram);
+        assert_eq!(answer, vec![
             ('A', '2'),
             ('B', '1'),
             ('C', '3'),
@@ -56,8 +78,6 @@ mod main_tests {
 
     #[test]
     fn example_2() {
-        let w = 18;
-        let h = 22;
         let diagram = vec![
             "P  Q  R  S  T  U  V  W".to_string(),
             "|  |  |  |  |--|  |  |".to_string(),
@@ -78,8 +98,8 @@ mod main_tests {
             "|--|  |--|  |  |--|  |".to_string(),
             "1  2  3  4  5  6  7  8".to_string(),
         ];
-        let answer = solve(w, h, diagram);
-        assert_eq!(answer.unwrap(), vec![
+        let answer = solve(&diagram);
+        assert_eq!(answer, vec![
             ('P', '3'),
             ('Q', '7'),
             ('R', '8'),
